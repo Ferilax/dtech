83e2cf1fd24cf0a76a573b294622d867b95bff67
Ferilax <muhammadfx@mail.ru>
2024-03-29 14:35:44 +0300

    poly

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,10 @@
 const coverInfo = document.querySelector(".cover__info");
 const optimizationSection = document.querySelector(".optimization");
 
+
 function smoothScroll(trigger, element) {
 	trigger.addEventListener("click", function (e) {
-		element.scrollIntoView({
-			behavior: 'smooth'
-		});
+		element.scrollIntoView();
 	});
 }
 
